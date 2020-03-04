d09aead8931f535ac344c38cdb45f07a2844fa46
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2020-03-04 07:54:21 -0600

    add sleep after game creation

--- views/new.php
+++ views/new.php
@@ -2,7 +2,7 @@
 $ppath = gila::base_url().'src/'.GPACKAGE.'/';
 $play_url = GPACKAGE.'/play';
 $pnk = new gTable('playerclass');
-$classes = $pnk->getRows();
+$classes = $pnk->getRows(null);
 ?>
 <link href="<?=$style_css_path?>" rel="stylesheet">
 <style>
